3e2a1b99a5d2624d2f661e326da442fcf1d81f21
Abhaybabar009 <abhaybabar781@gmail.com>
2025-02-05 23:46:31 -0800

    Add files via upload

---
+++ abhay.py
@@ -0,0 +1,16 @@
+ import numpy as np
+X = np.array([1, 2, 3, 4, 5,6 ])
+y = np.array([1.2, 1.9, 3.0, 3.8, 5.1, 5.3])
+w, b = 0, 0  # Start with 0
+learning_rate = 0.01
+iterations = 10
+m = len(y)
+for _ in range(iterations):
+    y_pred = w * X + b
+    errors = y_pred - y
+    dw = (1 / m) * np.dot(errors, X)
+    db = (1 / m) * np.sum(errors)
+    w -= learning_rate * dw
+    b -= learning_rate * db
+print(f"Weight (w): {w}, Bias (b): {b}")
+
